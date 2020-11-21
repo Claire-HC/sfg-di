85f988b107d4796f2bfd80f6c5fd5825ce74118a
Claire Chen <hche0040@student.monash.edu>
2020-11-21 21:54:23 +1100

    Adding primary bean examples

---
+++ src/main/java/claire/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,16 @@
+package claire.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by claire on 21/11/20
+ */
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the PRIMARY Bean";
+    }
+}
